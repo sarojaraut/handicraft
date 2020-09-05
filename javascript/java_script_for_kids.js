c4c52ef6adc8c06f6aaa8428aa63444f8ecde53b
Saroj Raut <saroj.raut@gmail.com>
2020-09-05 12:59:58 +0100

    java script

---
+++ javascript/java_script_for_kids.js
@@ -0,0 +1,77 @@
+/***** Chapter 2 - Data Types and Variables ****/
+var age = 12;
+var numberOfSiblings = 1 + 3;
+var numberOfCandies = 24, numberOfKids = 6;
+var secondsInAMinute = 60;
+var minutesInAnHour = 60;
+var secondsInAnHour = secondsInAMinute * minutesInAnHour;
+
+var highFives = 0;
+++highFives;//1  first increment then use
+highFives++; //1 first use then increment
+
+var score = 10;
+score += 7; // short hand for score = score + 7;
+
+var myAwesomeString = "Something REALLY awesome!!!";
+
+var numberNine = 9;
+var stringNine = "9";
+numberNine + numberNine; //18
+stringNine + stringNine; //"99"
+
+var java = "Java";
+java.length; //4
+
+var myName = "Saroj";
+myName[0]; // "S"
+
+var longString = "My long string is long";
+longString.slice(3, 14); //"long string"
+longString.slice(0, 2); // "My"
+longString.slice(8);//"string is long"
+
+myName.toUpperCase();//"SAROJ"
+
+var javascriptIsCool = true;
+
+var hadShower = true;
+var hasBackpack = false;
+hadShower && hasBackpack; //false
+|| (or)
+! (not)
+
+Double Equals “equal-ish.” 
+
+var stringNumber = "5";
+var actualNumber = 5;
+stringNumber === actualNumber; // false
+stringNumber == actualNumber; // true
+
+0 == false; //true
+"false" == false; //false
+
+//This is because when JavaScript tries to compare two values with double equals, it first tries to make them the same type. In this case, it converts the Boolean into a number. If you convert Booleans to numbers, false becomes 0 , and true becomes 1 .
+
+undefined and null
+
+They’re both used to mean “nothing,” but in slightly different ways. undefined is the value JavaScript uses when it doesn’t have a value for something. For example, when you create a new variable, if you don’t set its value to anything using the = operator, its value will be set to undefined.Double
+The null value is usually used when you want to deliberately say “This is empty.”
+
+/***** Chapter 3 - Arrays ****/
+
+
+
+
+
+
+
+/***** Chapter 2 - Data Types and Variables ****/
+/***** Chapter 2 - Data Types and Variables ****/
+/***** Chapter 2 - Data Types and Variables ****/
+/***** Chapter 2 - Data Types and Variables ****/
+/***** Chapter 2 - Data Types and Variables ****/
+/***** Chapter 2 - Data Types and Variables ****/
+/***** Chapter 2 - Data Types and Variables ****/
+/***** Chapter 2 - Data Types and Variables ****/
+/***** Chapter 2 - Data Types and Variables ****/
