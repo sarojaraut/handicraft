b63522d4dc5403b60421ad3e998fbec5c0a98776
Saroj Raut <saroj.raut@gmail.com>
2020-10-02 18:13:14 +0100

    more node code

---
+++ javascript/nodeBeginner/web-3/basicPostRequestParsed/requestHandlers.js
@@ -0,0 +1,33 @@
+//requestHandlers.js
+
+var exec = require("child_process").exec;
+// we just introduced a new Node.js module, child_process. We did so because it allows us to make use of a very simple yet useful non-blocking
+var querystring = require("querystring");
+
+function start(response) {
+    console.log("Request handler 'start' was called.")
+    var body = '<html>'+
+        '<head>'+
+        '<meta http-equiv="Content-Type" content="text/html; '+
+        'charset=UTF-8" />'+
+        '</head>'+
+        '<body>'+
+        '<form action="/upload" method="post">'+
+        '<textarea name="text" rows="20" cols="60"></textarea>'+
+        '<input type="submit" value="Submit text" />'+
+        '</form>'+
+        '</body>'+
+        '</html>';6
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(body);
+    response.end();
+}
+
+function upload(response, postData) {
+    console.log("Request handler 'upload' was called.");
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("You've sent: " + querystring.parse(postData).text);
+    response.end();
+}
+exports.start = start;
+exports.upload = upload;
---
+++ javascript/nodeBeginner/web-3/readme.txt
@@ -0,0 +1,11 @@
+
+http://localhost:8888/start
+http://localhost:8888/upload
+http://localhost:8888/notfound
+
+Serving something useful
+handle incoming POST requests (but not file uploads)
+We will present a textarea that can be filled by the user and submitted to the server in a POST request. Upon receiving and handling this request, we will display the content of the textarea.
+We'll not include an extra level of abstraction (i.e., separating view and controller logic) and will all add html code to ourrequest handler
+To make the whole process non-blocking, Node.js serves our code the POST data in small chunks, callbacks that are called upon certain events. These events are data (a new chunk of POST data arrives) and end (all chunks have been received).
+We need to tell Node.js which functions to call back to when these events occur. This is done by adding listeners to the request object that is passed to our onRequest callback whenever an HTTP request is received.
