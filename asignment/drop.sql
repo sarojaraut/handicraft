d292efa4eba92cc048fbca8ecddfdad6a71326ac
Saroj Raut <saroj.raut@gmail.com>
2020-10-05 14:18:22 +0100

    Interview Assignment

---
+++ asignment/drop.sql
@@ -0,0 +1,5 @@
+drop table my_order_event;
+drop table my_order_item;
+drop table my_order;
+drop table my order_status;
+drop table my_item;
---
+++ asignment/install.sql
@@ -0,0 +1,5 @@
+@my_item.sql
+@my_order_status.sql
+@my_order.sql
+@my_order_item.sql
+@my_order_event.sql
