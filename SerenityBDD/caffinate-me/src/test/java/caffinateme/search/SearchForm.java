fbb73ac40273a1aa0909b29591447a5ac4ed59b0
Saroj Raut <Saroj.raut@gmail.com>
2021-01-16 12:23:19 +0000

    BDD

---
+++ SerenityBDD/caffinate-me/src/test/java/caffinateme/search/SearchForm.java
@@ -0,0 +1,9 @@
+package caffinateme.search;
+
+import net.serenitybdd.screenplay.targets.Target;
+
+class SearchForm {
+    static Target SEARCH_FIELD = Target.the("search field")
+                                       .locatedBy("#searchInput");
+
+}
