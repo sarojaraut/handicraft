aa2f8f15dfdd504a93633fea94a5c10bb07532b9
Saroj Raut <Saroj.raut@gmail.com>
2021-02-27 06:20:25 +0000

    More

--- ORDSPractice/helth_check.sql
+++ ORDSPractice/helth_check.sql
@@ -8,6 +8,7 @@ end;
 /
 
 
+
 BEGIN
     ORDS.ENABLE_SCHEMA(
         p_enabled             => TRUE,
@@ -62,8 +63,108 @@ BEGIN
             from dual
         ]'
         );
+    -- Unprotected
+
+    ORDS.DEFINE_TEMPLATE(
+        p_module_name    => 'healthcheck.v1',
+        p_pattern        => 'v2/',
+        p_priority       => 0,
+        p_etag_type      => 'HASH',
+        p_etag_query     => NULL,
+        p_comments       => NULL);
+
+    ORDS.DEFINE_HANDLER(
+        p_module_name    => 'healthcheck.v1',
+        p_pattern        => 'v2/',
+        p_method         => 'GET',
+        p_source_type    => 'json/query',
+        p_items_per_page =>  0,
+        p_mimes_allowed  => '',
+        p_comments       => NULL,
+        p_source         =>q'[
+            select 
+                sys_context('userenv', 'db_name')                  db_name
+            from dual
+        ]'
+        );
 
     COMMIT;
-    -- URI http://server:port/ords/api/healthcheck/v1/
+    -- URI http://server:port/ords/api/healthcheck/v2/
+    --https://sd2cgxnc6zom7sw-ourcompany.adb.uk-london-1.oraclecloudapps.com/ords/api/healthcheck/v2/
 END;
 /
+
+begin
+    ords.create_role('healthcheck_role');     
+    commit;
+end;
+/
+
+DECLARE
+    l_arr owa.vc_arr;
+BEGIN
+    l_arr(1) := 'healthcheck_role';
+
+    ORDS.define_privilege (
+        p_privilege_name => 'healthcheck_priv',
+        p_roles          => l_arr,
+        p_label          => 'Health check Access Privilege',
+        p_description    => 'Allow access to healthcheck API'
+    );
+
+    COMMIT;
+END;
+/
+
+begin
+    ords.create_privilege_mapping(
+        p_privilege_name => 'healthcheck_priv',
+        p_pattern => '/healthcheck/v1/*');     
+    commit;
+end;
+/
+
+BEGIN
+    OAUTH.create_client(
+        p_name            => 'healthcheck_Client',
+        p_grant_type      => 'client_credentials',
+        p_owner           => 'Saroj',
+        p_description     => 'A client for healthcheck management',
+        p_support_email   => 'test@test.com',
+        p_privilege_names => 'healthcheck_priv'
+    );
+
+    COMMIT;
+END;
+/
+
+SELECT id, name, client_id, client_secret FROM user_ords_clients;
+
+client id : zMsOZn2pLkd6zrsNwsQ90Q..
+client secret : OKbAMNlB2h0EcCX0OnGLHw..
+
+-- Display client-privilege relationship.
+SELECT name, client_name FROM user_ords_client_privileges;
+
+-- Associate the client with org_role role required for org_priv
+BEGIN
+    OAUTH.grant_client_role(
+        p_client_name => 'healthcheck_Client',
+        p_role_name   => 'healthcheck_role'
+    );
+
+    COMMIT;
+END;
+/
+
+
+
+
+curl -i -k --user zMsOZn2pLkd6zrsNwsQ90Q..:OKbAMNlB2h0EcCX0OnGLHw.. --data "grant_type=client_credentials" https://sd2cgxnc6zom7sw-ourcompany.adb.uk-london-1.oraclecloudapps.com/ords/api/oauth/token
+
+curl --request GET \
+  --url https://sd2cgxnc6zom7sw-ourcompany.adb.uk-london-1.oraclecloudapps.com/ords/api/healthcheck/v1/ \
+  --header 'Content-Type: application/x-www-form-urlencoded'
+
+curl -i -k --user uny2QusgGI6hZJ6I-jzyCw..:kJ7oI7dRjljbXkNd52E4Ig.. --data "grant_type=client_credentials" https://sd2cgxnc6zom7sw-devclearing.adb.uk-london-1.oraclecloudapps.com/ords/api/oauth/token
+
