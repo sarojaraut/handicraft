0a2542b1d6db19f9c4247ce453e40d7d1ed960bc
Saroj Ranjan Raut <39634713+sarojaraut@users.noreply.github.com>
2025-05-10 06:46:45 +0100

    Update Date and Interval.sql with timezone details

--- PLSQL/Date and Interval.sql
+++ PLSQL/Date and Interval.sql
@@ -30,9 +30,9 @@ INTERVAL YEAR [(year_precision)] TO MONTH
 INTERVAL DAY [(day_precision)] TO SECOND [(fractional_seconds_precision)]
 
 The precision elements are defined as follows.
-•year_precision – The maximum number of digits in the year component of the interval, such that a precision of 3 limits the interval to a maximum of 999 years. The default value is 2.
-•day_precision – The maximum number of digits in the day component of the interval, such that a precision of 4 limits the interval to a maximum of 9999 days. The day precision can accept a value from 0 to 9, with the default value being 2.
-•fraction_second_precision – The number of digits in the fractional component of the interval. Values between 0 and 9 are allowed, with the default value being 6.
+â€¢year_precision â€“ The maximum number of digits in the year component of the interval, such that a precision of 3 limits the interval to a maximum of 999 years. The default value is 2.
+â€¢day_precision â€“ The maximum number of digits in the day component of the interval, such that a precision of 4 limits the interval to a maximum of 9999 days. The day precision can accept a value from 0 to 9, with the default value being 2.
+â€¢fraction_second_precision â€“ The number of digits in the fractional component of the interval. Values between 0 and 9 are allowed, with the default value being 6.
 
 select sysdate + INTERVAL '2-2' YEAR TO MONTH from dual
 
@@ -78,4 +78,81 @@ SELECT EXTRACT(YEAR FROM systimestamp) FROM   dual;
 SELECT EXTRACT(HOUR FROM sysdate) FROM   dual; -- Invalid extract field for extract source
 SELECT EXTRACT(HOUR FROM systimestamp) FROM   dual; -- Invalid extract field for extract source
 
+select * from NLS_SESSION_PARAMETERS where parameter='NLS_TIMESTAMP_TZ_FORMAT';
+
+select 
+    systimestamp, 
+    systimestamp at time zone 'UTC' utc, 
+    systimestamp at time zone '+01:00' bst, 
+    current_timestamp, 
+    localtimestamp, 
+    sessiontimezone, 
+    dbtimezone 
+from dual;
+-- Output format depends on NLS_TIMESTAMP_TZ_FORMAT
+-- systimestamp: The return type is TIMESTAMP WITH TIME ZONE.
+-- current_timestamp: current date and time in the session time zone, in timestamp with time zone format
+-- localtimestamp: returns a TIMESTAMP value in session timezone
+-- sessiontimezone: ckient session timezone
+-- dbtimezone: database timezone as specified during create database
+
+-- TZR is timezone region , TZD is timezone daylight saving
+select 
+    to_char(systimestamp, 'yyyy-mm-dd hh24:mi:ss TZR TZD') sys_ts, 
+    to_char(systimestamp at time zone 'Asia/Kolkata', 'yyyy-mm-dd hh24:mi:ss TZR:TZD')  tzd_kol,
+    to_char(current_timestamp, 'yyyy-mm-dd hh24:mi:ss TZR TZD')  current_ts,
+    to_char(TO_TIMESTAMP_TZ('2022-11-25 11:00:00 -04:00','YYYY-MM-DD HH24:MI:SS TZH:TZM'),'yyyy-mm-dd hh24:mi:ss TZR TZD')  tzh_tzm,
+    to_char(TO_TIMESTAMP_TZ('2022-11-01 11:00:00 US/Pacific','YYYY-MM-DD HH24:MI:SS TZR'),'yyyy-mm-dd hh24:mi:ss TZR TZD')  tzd_pacific_pdt,
+    to_char(TO_TIMESTAMP_TZ('2022-11-01 11:00:00 US/Eastern','YYYY-MM-DD HH24:MI:SS TZR'),'yyyy-mm-dd hh24:mi:ss TZR TZD')  tzd_eastern_edt,
+    to_char(TO_TIMESTAMP_TZ('2022-11-01 11:00:00 US/Pacific','YYYY-MM-DD HH24:MI:SS TZR'),'yyyy-mm-dd hh24:mi:ss TZR TZD')  tzd_pacific_pst,
+    to_char(TO_TIMESTAMP_TZ('2022-11-01 11:00:00 US/Eastern','YYYY-MM-DD HH24:MI:SS TZR'),'yyyy-mm-dd hh24:mi:ss TZR TZD')  tzd_eastern_est,
+    to_char(TO_TIMESTAMP_TZ('2015-11-01 11:00:00 US/Pacific','YYYY-MM-DD HH24:MI:SS TZR'),'yyyy-mm-dd hh24:mi:ss TZR TZD')  tzd_pacific_2015,
+    to_char(TO_TIMESTAMP_TZ('2015-11-01 11:00:00 US/Eastern','YYYY-MM-DD HH24:MI:SS TZR'),'yyyy-mm-dd hh24:mi:ss TZR TZD')  tzd_eastern_2015, 
+    to_char(TO_TIMESTAMP_TZ('2022-10-01 11:00:00 Europe/London','YYYY-MM-DD HH24:MI:SS TZR'), 'yyyy-mm-dd hh24:mi:ss TZR TZD')  tzd_lon_oct_bst,
+    to_char(TO_TIMESTAMP_TZ('2022-12-01 11:00:00 Europe/London','YYYY-MM-DD HH24:MI:SS TZR'), 'yyyy-mm-dd hh24:mi:ss TZR TZD')  tzd_lon_dec_gmt,
+    to_char(TO_TIMESTAMP_TZ('2022-12-01 11:00:00 America/New_York','YYYY-MM-DD HH24:MI:SS TZR'), 'yyyy-mm-dd hh24:mi:ss TZR TZD')  tzd_ny_dec,
+    1 ignore_this
+from dual;
+
+Data Types
+DATE : This is the classic we all know and have used for years
+
+TIMESTAMP : Like DATE, TIMESTAMP does not include time zone information, so it also does not describe a moment in time.
+TIMESTAMP(0) is essentially the same as DATE
+TIMESTAMP(6) with a millionth of a second accuracy is the highest precision that most operating systems support
+
+TIMESTAMP WITH TIME ZONE (TSTZ): Here, we have a datatype that captures a moment in time. Because TSTZ includes the date + time (optionally to the fraction of a second) + time zone, 
+ it does capture a moment in time. The ability to capture a moment in time is critical when scheduling anything, and recording most actions (created_date, publish_date, etc.).
+TSTZ, however, really should only be used if knowing the time zone that something occurred in is important information.
+In most cases, if we normalize all of our TIMESTAMPs to UTC (or any arbitrary time zone), that is sufficient. 
+
+
+TIMESTAMP WITH LOCAL TIME ZONE (TSLTZ): Finally, the datatype we have all been waiting for! TSLTZ automatically normalizes the TIMESTAMP to the time zone of the database when it stores the data, 
+ and converts it back to the session time zone when it is selected out of the database.
+
+declare
+
+l_tsltz timestamp with local time zone := systimestamp;
+begin
+  dbms_output.put_line('Database:        ' || to_char(l_tsltz,'YYYY-MM-DD HH24-MI-SS TZR TZD'));
+
+  execute immediate 'alter session set time_zone=''US/Eastern''';
+  dbms_output.put_line('US/Eastern:      ' || to_char(l_tsltz,'YYYY-MM-DD HH24-MI-SS TZR TZD'));
+
+  execute immediate 'alter session set time_zone=''AMERICA/CHICAGO''';
+  dbms_output.put_line('AMERICA/CHICAGO: ' || to_char(l_tsltz,'YYYY-MM-DD HH24-MI-SS TZR TZD'));
+
+  execute immediate 'alter session set time_zone=''US/PACIFIC''';
+  dbms_output.put_line('US/PACIFIC:      ' || to_char(l_tsltz,'YYYY-MM-DD HH24-MI-SS TZR TZD'));
+end;
+/
+
+Database:        2022-10-27 10-19-37 AMERICA/NEW_YORK EDT
+US/Eastern:      2022-10-27 10-19-37 US/EASTERN EDT
+AMERICA/CHICAGO: 2022-10-27 09-19-37 AMERICA/CHICAGO CDT
+US/PACIFIC:      2022-10-27 07-19-37 US/PACIFIC PDT
+
+The TIMESTAMP WITH LOCAL TIME ZONE datatype normalizes to the database time zone. Hence there is no need to store a time zone with the dataâ€“it is, by definition, the database time zone.
+
+Oracle recommends that the database time zone be set to UTC (and never changed). In practice, it does not matter what the database time zone is, so long as it never changes. TIMESTAMP WITH LOCAL TIME ZONE  data will always normalize on the way into the field and convert to the session time zone on the way out.
 
